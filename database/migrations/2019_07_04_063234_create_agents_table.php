3bd437d45df2e829a939f5989e9385fd39406ce7
Mboizi <mboizi@localhost.localdomain>
2019-07-04 15:16:01 +0300

    agents_table

---
+++ database/migrations/2019_07_04_063234_create_agents_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAgentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agents', function (Blueprint $table) {
+            $table->bigIncrements('agentid');
+            $table->string('fname');
+            $table->string('lname');
+            $table->string('date');
+            $table->string('tel');
+            $table->string('email');
+            $table->string('district');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agents');
+    }
+}
